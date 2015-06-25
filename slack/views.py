3951a14e90c3d5736557575bf1a2c6b446d390bb
nicolewhite <nicole.white@neotechnology.com>
2015-06-24 20:25:04 -0700

    URL decoding: need to decode, not encode.

--- slack/views.py
+++ slack/views.py
@@ -1,11 +1,12 @@
 from flask import Flask, request
 import requests
-from urllib import urlencode
+from urllib import unquote
 
 app = Flask(__name__)
 
 @app.route("/")
 def meme():
+    domain = request.args["team_domain"]
     slackbot = request.args["slackbot"]
     text = request.args["text"]
     channel = request.args["channel_name"]
@@ -13,7 +14,7 @@ def meme():
     text = text[:-1] if text[-1] == ";" else text
     params = text.split(";")
     params = [x.strip().replace(" ", "-") for x in params]
-    params = [urlencode(x) for x in params]
+    params = [unquote(x) for x in params]
 
     if not len(params) == 3:
         response = "Your syntax should be in the form: /meme template; top; bottom;"
@@ -24,5 +25,5 @@ def meme():
 
         response = "http://memegen.link/{0}/{1}/{2}.jpg".format(template, top, bottom)
 
-    url = "https://neo4j.slack.com/services/hooks/slackbot?token={0}&channel=%23{1}".format(slackbot, channel)
+    url = "https://{0}.slack.com/services/hooks/slackbot?token={1}&channel=%23{2}".format(domain, slackbot, channel)
     requests.post(url, data=response)
